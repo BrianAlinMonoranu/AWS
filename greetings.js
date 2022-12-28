53ccd65cd859bb1c2eb1656646cdfc8aa6733930
Brian Alin Monoranu <71988258+BrianAlinMonoranu@users.noreply.github.com>
2022-12-28 14:40:12 +0000

    Update greetings.js

--- greetings.js
+++ greetings.js
@@ -1,15 +1,23 @@
 const greeting = {
-    "en": "Hello",
-    "fr": "Bonjour",
-    "hi": "Namaste",
-    "es": "Hola"
-}
+    en: "Hello",
+    fr: "Bonjour",
+    hi: "Namaste",
+    es: "Hola",
+};
 
+//async means that everything else can also be running while this function runs
 export const handler = async(event) => {
     let name = event.pathParameters.name;
     let { lang, ...info } = event.queryStringParameters;
-    let message = `${greeting[lang]? greeting[lang] : greeting['en']} ${name}`
+    let message = `${greeting[lang] ? greeting[lang] : greeting["en"]} ${name}`;
+    let response = {
+        message: message,
+        info: info,
+    };
+
+    //This is for good HTTP response instead of just returning response
+    return {
+        statusCode: 200,
+        body: JSON.stringify(response),
+    };
 };
-
-
-//It will print out hello in a certain language and then the name of the user
